f752cd45b72a748933b82181253faf65d5078b3d
k.khamidov <kamolkhamidov200002@gmail.com>
2023-05-18 13:48:46 +0500

    feat: add pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	apps: [
+		{
+			name: 'bot',
+			script: './dist/index.js',
+			instances: 1,
+			autorestart: true,
+			watch: false,
+			max_memory_restart: '1G',
+			env: {
+				NODE_ENV: 'production'
+			}
+		}
+	]
+};
