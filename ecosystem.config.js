2d3357d757bf0da66c96528a7956984857da6984
k.khamidov <kamolkhamidov200002@gmail.com>
2023-06-03 03:33:29 +0500

    update pm2 name

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-	apps: [
-		{
-			name: 'twitter-bot',
-			script: './dist/index.js',
-			instances: 1,
-			autorestart: true,
-			watch: false,
-			max_memory_restart: '1G',
-			env: {
-				NODE_ENV: 'production'
-			}
-		}
-	]
+    apps: [
+        {
+            name: 'social-media-bot',
+            script: './dist/index.js',
+            instances: 1,
+            autorestart: true,
+            watch: false,
+            max_memory_restart: '1G',
+            env: {
+                NODE_ENV: 'production',
+            },
+        },
+    ],
 };
